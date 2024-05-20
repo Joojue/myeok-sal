c59041200519bf806dee3cd3e7c94ab5a645bd3e
Joojue <128790356+Joojue@users.noreply.github.com>
2024-05-20 13:54:13 +0900

    완성

--- 고다짐/godagym-myeoksal/src/App.tsx
+++ 고다짐/godagym-myeoksal/src/App.tsx
@@ -1,10 +1,291 @@
-import "./App.css";
-
 function App() {
   return (
-    <>
-      <div></div>
-    </>
+    <div
+      style={{
+        backgroundColor: "black",
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "1rem",
+        boxSizing: "border-box",
+      }}
+    >
+      <div
+        style={{
+          borderRadius: "10px",
+          width: "100%",
+          minWidth: "350px",
+          height: "600px",
+          backgroundColor: "white",
+          padding: "1rem",
+          boxSizing: "border-box",
+        }}
+      >
+        <h3 style={{ textAlign: "center" }}>빼바오 '오로지 팀성공'</h3>
+        <div
+          style={{
+            justifyContent: "center",
+            fontWeight: 700,
+            display: "flex",
+            alignItems: "center",
+            gap: "0.5rem",
+            fontSize: "0.875rem",
+            marginBottom: "1rem",
+          }}
+        >
+          <div style={{ flex: 1, textAlign: "center" }}>팀원</div>
+          <div style={{ flex: 1, textAlign: "center" }}>1주차</div>
+          <div style={{ flex: 1, textAlign: "center" }}>2주차</div>
+          <div style={{ flex: 1, textAlign: "center" }}>3주차</div>
+          <div style={{ flex: 1, textAlign: "center" }}>4주차</div>
+          <div
+            style={{
+              flex: 1,
+              whiteSpace: "pre-wrap",
+              textAlign: "center",
+              fontSize: "0.775rem",
+            }}
+          >{`남은\n체지방`}</div>
+          <div style={{ flex: 1, textAlign: "center" }}>상태</div>
+        </div>
+        <div
+          style={{
+            justifyContent: "center",
+            marginBottom: "1rem",
+            display: "flex",
+            alignItems: "center",
+            gap: "0.5rem",
+            fontSize: "0.875rem",
+          }}
+        >
+          <div style={{ flex: 1, textAlign: "center", fontSize: "0.8rem" }}>
+            최은영
+          </div>
+          <div style={{ flex: 1, textAlign: "center" }}></div>
+          <div style={{ flex: 1, textAlign: "center" }}></div>
+          <div style={{ flex: 1, textAlign: "center" }}></div>
+          <div style={{ flex: 1, textAlign: "center" }}></div>
+          <div
+            style={{
+              flex: 1,
+              whiteSpace: "pre-wrap",
+              textAlign: "center",
+            }}
+          >
+            3.6
+          </div>
+          <div
+            style={{
+              flex: 1,
+              textAlign: "center",
+              fontSize: "0.675rem",
+              backgroundColor: "#fff3f3",
+              color: "#FF6060",
+            }}
+          >
+            도전중..
+          </div>
+        </div>
+        <div
+          style={{
+            justifyContent: "center",
+            marginBottom: "1rem",
+            display: "flex",
+            alignItems: "center",
+            gap: "0.5rem",
+            fontSize: "0.875rem",
+          }}
+        >
+          <div style={{ flex: 1, textAlign: "center", fontSize: "0.8rem" }}>
+            차가든
+          </div>
+          <div style={{ flex: 1, textAlign: "center" }}></div>
+          <div style={{ flex: 1, textAlign: "center" }}></div>
+          <div style={{ flex: 1, textAlign: "center" }}></div>
+          <div style={{ flex: 1, textAlign: "center" }}></div>
+          <div
+            style={{
+              flex: 1,
+              whiteSpace: "pre-wrap",
+              textAlign: "center",
+            }}
+          >
+            3.7
+          </div>
+          <div
+            style={{
+              flex: 1,
+              textAlign: "center",
+              fontSize: "0.675rem",
+              backgroundColor: "#fff3f3",
+              color: "#FF6060",
+            }}
+          >
+            도전중..
+          </div>
+        </div>
+        <div
+          style={{
+            justifyContent: "center",
+            marginBottom: "1rem",
+            display: "flex",
+            alignItems: "center",
+            gap: "0.5rem",
+            fontSize: "0.875rem",
+          }}
+        >
+          <div style={{ flex: 1, textAlign: "center", fontSize: "0.8rem" }}>
+            차지민
+          </div>
+          <div style={{ flex: 1, textAlign: "center" }}></div>
+          <div style={{ flex: 1, textAlign: "center" }}></div>
+          <div style={{ flex: 1, textAlign: "center" }}></div>
+          <div style={{ flex: 1, textAlign: "center" }}></div>
+          <div
+            style={{
+              flex: 1,
+              whiteSpace: "pre-wrap",
+              textAlign: "center",
+            }}
+          >
+            3.2
+          </div>
+          <div
+            style={{
+              flex: 1,
+              textAlign: "center",
+              fontSize: "0.675rem",
+              backgroundColor: "#fff3f3",
+              color: "#FF6060",
+            }}
+          >
+            도전중..
+          </div>
+        </div>
+        <div
+          style={{
+            justifyContent: "center",
+            marginBottom: "1rem",
+            display: "flex",
+            alignItems: "center",
+            gap: "0.5rem",
+            fontSize: "0.875rem",
+          }}
+        >
+          <div style={{ flex: 1, textAlign: "center", fontSize: "0.8rem" }}>
+            김종훈
+          </div>
+          <div style={{ flex: 1, textAlign: "center" }}></div>
+          <div style={{ flex: 1, textAlign: "center" }}></div>
+          <div style={{ flex: 1, textAlign: "center" }}></div>
+          <div style={{ flex: 1, textAlign: "center" }}></div>
+          <div
+            style={{
+              flex: 1,
+              whiteSpace: "pre-wrap",
+              textAlign: "center",
+            }}
+          >
+            5.7
+          </div>
+          <div
+            style={{
+              flex: 1,
+              textAlign: "center",
+              fontSize: "0.675rem",
+              backgroundColor: "#fff3f3",
+              color: "#FF6060",
+            }}
+          >
+            도전중..
+          </div>
+        </div>
+        <div
+          style={{
+            justifyContent: "center",
+            marginBottom: "1rem",
+            display: "flex",
+            alignItems: "center",
+            gap: "0.5rem",
+            fontSize: "0.875rem",
+          }}
+        >
+          <div style={{ flex: 1, textAlign: "center", fontSize: "0.8rem" }}>
+            김지우
+          </div>
+          <div style={{ flex: 1, textAlign: "center" }}></div>
+          <div style={{ flex: 1, textAlign: "center" }}></div>
+          <div style={{ flex: 1, textAlign: "center" }}></div>
+          <div style={{ flex: 1, textAlign: "center" }}></div>
+          <div
+            style={{
+              flex: 1,
+              whiteSpace: "pre-wrap",
+              textAlign: "center",
+            }}
+          >
+            4.1
+          </div>
+          <div
+            style={{
+              flex: 1,
+              textAlign: "center",
+              fontSize: "0.675rem",
+              backgroundColor: "#fff3f3",
+              color: "#FF6060",
+            }}
+          >
+            도전중..
+          </div>
+        </div>
+        <div
+          style={{
+            justifyContent: "center",
+            marginBottom: "1rem",
+            display: "flex",
+            alignItems: "center",
+            gap: "0.5rem",
+            fontSize: "0.875rem",
+          }}
+        >
+          <div style={{ flex: 1, textAlign: "center", fontSize: "0.8rem" }}>
+            윤혜선
+          </div>
+          <div style={{ flex: 1, textAlign: "center" }}></div>
+          <div style={{ flex: 1, textAlign: "center" }}></div>
+          <div style={{ flex: 1, textAlign: "center" }}></div>
+          <div style={{ flex: 1, textAlign: "center" }}></div>
+          <div
+            style={{
+              flex: 1,
+              whiteSpace: "pre-wrap",
+              textAlign: "center",
+            }}
+          >
+            4.7
+          </div>
+          <div
+            style={{
+              flex: 1,
+              textAlign: "center",
+              fontSize: "0.675rem",
+              backgroundColor: "#fff3f3",
+              color: "#FF6060",
+            }}
+          >
+            도전중..
+          </div>
+        </div>
+        <div style={{ marginTop: "3rem" }}>
+          <h2 style={{ textAlign: "center" }}>목표 감량 체지방량 25kg</h2>
+        </div>
+        <div style={{ marginTop: "1.5rem" }}>
+          <h2 style={{ textAlign: "center" }}>남은 체지방량 25kg</h2>
+        </div>
+      </div>
+    </div>
   );
 }
 
